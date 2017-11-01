41f19ab7ef2fe1dad78ceb1bccc72bb95e2e6172
Cameron Powers <powerscameron@ymail.com>
2017-10-31 18:21:45 -0600

    Functionality Work

--- mushroom_app/static/js_files/img_functions.js
+++ mushroom_app/static/js_files/img_functions.js
@@ -13,27 +13,27 @@ function getValue(){
 
 function placeImages(json_lst, min){
     var oder = {
-        images = new Array("odor_almond.jpeg", "odor_anise.jpeg", "odor_creosote.jpg", "odor_fishy.jpg", "odor_foul.jpg", "odor_musty.jpeg", "odor_none.jpeg", "odor_pungent.jpg", "odor_spicy .jpg"),
-        values = new Array("a", "l", "c", "y", "f", "m", "n", "p", "s"),
-        text = new Array("Almond", "Anise", "Creosote", "Fishy", "Foul", "Musty", "None", "Pungent", "Spicy")
+        images : ["odor_almond.jpeg", "odor_anise.jpeg", "odor_creosote.jpg", "odor_fishy.jpg", "odor_foul.jpg", "odor_musty.jpeg", "odor_none.jpeg", "odor_pungent.jpg", "odor_spicy .jpg"],
+        values : ["a", "l", "c", "y", "f", "m", "n", "p", "s"],
+        text : ["Almond", "Anise", "Creosote", "Fishy", "Foul", "Musty", "None", "Pungent", "Spicy"]
     };
 
     var gill_size = {
-        images = new Array("broad_gillsize.jpeg", "narrow_gillsize.jpeg"),
-        values = new Array("b", "n"),
-        text = new Array("Broad gill-size", "Narrow gill-size")
+        images : ["broad_gillsize.jpeg", "narrow_gillsize.jpeg"],
+        values : ["b", "n"],
+        text : ["Broad gill-size", "Narrow gill-size"]
     };
 
     var spore_print_color = {
-        images = new Array("spc_black.jpg","spc_brown.jpg", "spc_buff.jpg", "spc_chocolate.jpeg", "spc_green.png", "spc_orange.jpeg", "spc_purple.jpeg", "spc_white.jpeg", "spc_yellow.jpg"),
-        values = new Array("k", "n", "b", "h", "r", "o", "u", "w", "y"),
-        text = new Array("Black", "Brown", "Buff", "Chocolate", "Green", "Orange", "Purple", "White", "Yellow")
+        images : ["spc_black.jpg","spc_brown.jpg", "spc_buff.jpg", "spc_chocolate.jpeg", "spc_green.png", "spc_orange.jpeg", "spc_purple.jpeg", "spc_white.jpeg", "spc_yellow.jpg"],
+        values : ["k", "n", "b", "h", "r", "o", "u", "w", "y"],
+        text : ["Black", "Brown", "Buff", "Chocolate", "Green", "Orange", "Purple", "White", "Yellow"]
     };
 
     var habitat = {
-        images = new Array("hab_grasses.jpeg", "hab_leaves.jpeg", "hab_meadow.jpg", "hab_path.jpg", "hab_urban.jpg", "hab_waste.jpeg", "hab_woods.jpg"),
-        values = new Array("g", "l", "m", "p", "u", "w", "d"),
-        text = new Array("Grasses", "Leaves", "Meadows", "Paths", "Urban", "Waste", "Woods"),
+        images : ["hab_grasses.jpeg", "hab_leaves.jpeg", "hab_meadow.jpg", "hab_path.jpg", "hab_urban.jpg", "hab_waste.jpeg", "hab_woods.jpg"],
+        values : ["g", "l", "m", "p", "u", "w", "d"],
+        text : ["Grasses", "Leaves", "Meadows", "Paths", "Urban", "Waste", "Woods"]
     };
 
 
@@ -41,7 +41,7 @@ function placeImages(json_lst, min){
 
     if(min <= max){
 
-        for (i = 0; i <= eval(json_lst)[min].images.length - 1; ++1){
+        for (i = 0; i <= eval(json_lst)[min].images.length - 1; i++){
             var opt = document.createElement("option");
             opt.setAttribute('value') = json_lst[min].values[i]
             opt.setAttribute('data-img-src') = '~/DS_practice/mushroom_project/mushroom_app/static/feature_imgs' + json_lst[min].images[i]
@@ -49,19 +49,26 @@ function placeImages(json_lst, min){
         };
         min = min + 1
         return min
-    };
-    else{
-        alert("You have made a selection for each parameter. Please hit the submit button. ")
-    };
+    }
+    else {
+        alert("You have made a selection for each parameter. Please hit the submit button. ");
+    }
 }
 
 function removeOptions(select){
-    var i;
-    for (i = select.options.length - 1; i >= 0; i-- )
-    {
-        select.remove(i);
-    }
+    $(document).ready(function(){
+    $("select").imagepicker({
+        selected:function(option){
+            alert(select.options.length)
+        var i;
+        for (i = select.options.length - 1; i >= 0; i-- )
+                {
+                    select.remove(i);
+                }
+            }
 
+        });
+    });
 }
 
 
