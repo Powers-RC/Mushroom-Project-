5df4d00465a2f641c35aa0968d103920a4acc922
Cameron Powers <powerscameron@ymail.com>
2017-11-21 12:16:17 -0700

    Single onClick Function

--- mushroom_app/static/js_files/img_functions.js
+++ mushroom_app/static/js_files/img_functions.js
@@ -1,3 +1,7 @@
+var feature_lst = new Array("oder", "gill_size", "spore_print_color", "habitat");
+
+var min = 0;
+
 // add if statement for the length of container
 function getValue(){
     $(document).ready(function(){
@@ -76,4 +80,66 @@ function removeOptions(select){
 }
 
 
-var feature_lst = new Array("oder", "gill_size", "spore_print_color", "habitat");
+function onClick(json_lst, min){
+    $(document).ready(function(){
+        var oder = {
+            images : ["odor_almond.jpeg", "odor_anise.jpeg", "odor_creosote.jpg", "odor_fishy.jpg", "odor_foul.jpg", "odor_musty.jpeg", "odor_none.jpeg", "odor_pungent.jpg", "odor_spicy .jpg"],
+            values : ["a", "l", "c", "y", "f", "m", "n", "p", "s"],
+            text : ["Almond", "Anise", "Creosote", "Fishy", "Foul", "Musty", "None", "Pungent", "Spicy"]
+        };
+
+        var gill_size = {
+            images : ["broad_gillsize.jpeg", "narrow_gillsize.jpeg"],
+            values : ["b", "n"],
+            text : ["Broad gill-size", "Narrow gill-size"]
+        };
+
+        var spore_print_color = {
+            images : ["spc_black.jpg","spc_brown.jpg", "spc_buff.jpg", "spc_chocolate.jpeg", "spc_green.png", "spc_orange.jpeg", "spc_purple.jpeg", "spc_white.jpeg", "spc_yellow.jpg"],
+            values : ["k", "n", "b", "h", "r", "o", "u", "w", "y"],
+            text : ["Black", "Brown", "Buff", "Chocolate", "Green", "Orange", "Purple", "White", "Yellow"]
+        };
+
+        var habitat = {
+            images : ["hab_grasses.jpeg", "hab_leaves.jpeg", "hab_meadow.jpg", "hab_path.jpg", "hab_urban.jpg", "hab_waste.jpeg", "hab_woods.jpg"],
+            values : ["g", "l", "m", "p", "u", "w", "d"],
+            text : ["Grasses", "Leaves", "Meadows", "Paths", "Urban", "Waste", "Woods"]
+        };
+
+        var container = []
+
+        $("select").imagepicker({
+            clicked:function(){
+                //Sends image value
+                var value = this.val();
+                container.push(value);
+                //Remove Options
+                document.getElementsByClassName('picker')[0].options.length = 0
+                //Adds New Options
+                var max = json_lst.length - 1, select = document.getElementsByClassName('picker')[0]
+
+                if(min <= max){
+                    console.log(min)
+                    for (i = 0; i <= eval(eval(json_lst)[min]).images.length - 1; i++){
+                        var opt = document.createElement("option");
+                        opt.setAttribute('value', eval(json_lst[min]).values[i]);
+                        opt.setAttribute('data-img-src', '../static/feature_imgs/' + eval(json_lst[min]).images[i]);
+                        opt.setAttribute('innerHTML', eval(json_lst[min]).text[i]);
+                        select.appendChild(opt);
+                        // $('select').imagepicker();
+                                    }
+                                $('select').imagepicker({
+                                    clicked:onclick()
+                                });
+                                min = min + 1
+                                console.log(min)
+                                return min
+                                }
+                    else {
+                        alert("You have made a selection for each parameter. Please hit the submit button. ");
+                        }
+
+                }
+            });
+        });
+    }
